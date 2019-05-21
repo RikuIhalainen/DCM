b0dca1aef6cb98be6e8504c459047b2f9e5a412b
Riku <32592642+Aavistus@users.noreply.github.com>
2019-05-21 21:11:29 +0100

    updates

--- rundcm.m
+++ rundcm.m
@@ -12,6 +12,7 @@ spm('Defaults','EEG'); % SPM-EEG modality interface, default settings
 %--------------------------------------------------------------------------
 % Location priors for dipoles - names and prior mean locations 
 %--------------------------------------------------------------------------
+%------------------------
 if modindex == 1
 locs = {
     [-46 -66 30]  'lLPdmn'      %1.
@@ -58,13 +59,48 @@ dmPFC = 3;
 laPFCcen = 4;
 raPFCcen = 5;
 %------------------------
+elseif modindex == 4
+locs = {
+    [-46 -66 30]  'lLPdmn'      %1.
+    [49 -63 33]   'rLPdmn'      %2.
+    [0 -52 7]     'Prec/PCC'    %3.
+    [-1 54 27]    'mPFC'        %4.
+    [-62 -45 30]  'lLPsal'      %5.
+    [62 -45 30]   'rLPsal'      %6.
+    [0 21 36]     'dACC'        %7.
+    [-35 45 30]   'laPFCsal'    %8.
+    [32 45 30]    'raPFCsal'    %9.
+    [-50 -51 45]  'lSP'         %10.
+    [50 -51 45]   'rSP'         %11.
+    [0 24 46]     'dmPFC'       %12.
+    [-44 45 0]    'laPFCcen'    %13.
+    [44 45 0]     'raPFCcen'    %14.
+        };
+
+DCM.Sname = locs(:,2);
+
+lLPdmn = 1;
+rLPdmn = 2;
+Prec = 3;
+mPFC = 4;
+lLPsal = 5;
+rLPsal = 6;
+dACC = 7;
+laPFCsal = 8;
+raPFCsal = 9;
+lSP = 10;
+rSP = 11;
+dmPFC = 12;
+laPFCcen = 13;
+raPFCcen = 14;
+%------------------------
 end
 
 forward = 1;
 backward = 2;
 lateral = 3;
 DCM.Lpos = cell2mat(locs(:,1))';
-Nareas    = size(locs,1); 
+Nareas = size(locs,1); 
 
 
 %% Specify connectivity model - specification of the neuronal model
@@ -73,6 +109,7 @@ models = {
         'DMN'
         'SAL'
         'CEN'
+        'Full'
            };
 
 %DMN
@@ -97,7 +134,9 @@ if modindex == 1
     DCM.A{lateral}(lLPdmn,rLPdmn) = 1; %rLPdmn lateral connection to lLPdmn
     
     %Every connection is allowed to change
-    DCM.B{1} = DCM.A{1} + DCM.A{2} + DCM.A{3};     
+    DCM.B{1} = DCM.A{1} + DCM.A{2} + DCM.A{3};
+    DCM.C = sparse(length(DCM.A{1}),0); %Added C so no error in PEB
+    
 %-------------------------------------------------------    
 %SALIENCE
 elseif modindex == 2
@@ -126,6 +165,8 @@ elseif modindex == 2
     
     %Every connection is allowed to change
     DCM.B{1} = DCM.A{1} + DCM.A{2} + DCM.A{3};
+    DCM.C = sparse(length(DCM.A{1}),0); %Added C so no error in PEB
+    
 %-------------------------------------------------------
 % CENTRAL EXECUTIVE
 elseif modindex == 3 
@@ -154,8 +195,76 @@ elseif modindex == 3
     
     %Every connection is allowed to change
     DCM.B{1} = DCM.A{1} + DCM.A{2} + DCM.A{3};
-
+    DCM.C = sparse(length(DCM.A{1}),0); %Added C so no error in PEB
+%-------------------------------------------------------
+% FULL MODEL
+elseif modindex == 4
+   
+    DCM.A{forward} = zeros(Nareas,Nareas);
+    DCM.A{forward}(Prec,lLPdmn) = 1; %lLPdmn forward connection on Prec
+    DCM.A{forward}(mPFC,lLPdmn) = 1; %lLPdmn forward connection on mPFC
+    DCM.A{forward}(Prec,rLPdmn) = 1; %rLPdmn forward connection on Prec
+    DCM.A{forward}(mPFC,rLPdmn) = 1; %rLPdmn forward connection on mPFC
+    DCM.A{forward}(mPFC,Prec) = 1; %Prec forward connection on mPFC
+    DCM.A{forward}(dACC,lLPsal) = 1; %lLPsal forward connection on dACC
+    DCM.A{forward}(laPFCsal,lLPsal) = 1; %lLPsal forward connection on laPFCsal
+    DCM.A{forward}(dACC,rLPsal) = 1; %rLPsal forward connection on dACC
+    DCM.A{forward}(raPFCsal,rLPsal) = 1; %rLPsal forward connection on raPFCsal
+    DCM.A{forward}(dACC,laPFCsal) = 1; %laPFCsal forward connection on dACC
+    DCM.A{forward}(dACC,raPFCsal) = 1; %raPFCsal forward connection on dACC 
+    DCM.A{forward}(laPFCcen,lSP) = 1; %lSP forward connection on laPFCcen
+    DCM.A{forward}(dmPFC,lSP) = 1; %lSP forward connection on dmPFC
+    DCM.A{forward}(raPFCcen,rSP) = 1; %rSP forward connection on raPFCcen
+    DCM.A{forward}(dmPFC,rSP) = 1; %rSP forward connection on dmPFC
+    DCM.A{forward}(dmPFC,laPFCcen) = 1; %laPFCcen forward connection on dmPFC
+    DCM.A{forward}(dmPFC,raPFCcen) = 1; %raPFCcen forward connection on dmPFC
+    
+    DCM.A{backward} = zeros(Nareas,Nareas);
+    DCM.A{backward}(lLPdmn,Prec) = 1; %Prec backward connection on lLPdmn
+    DCM.A{backward}(rLPdmn,Prec) = 1; %Prec backward connection on rLPdmn
+    DCM.A{backward}(lLPdmn,mPFC) = 1; %mPFC backward connection on lLPdmn
+    DCM.A{backward}(rLPdmn,mPFC) = 1; %mPFC backward connection on rLPdmn
+    DCM.A{backward}(Prec,mPFC) = 1; %mPFC backward connection on Prec
+    DCM.A{backward}(lLPsal,dACC) = 1; %dACC backward connection on lLPsal
+    DCM.A{backward}(rLPsal,dACC) = 1; %dACC backward connection on rLPsal
+    DCM.A{backward}(laPFCsal,dACC) = 1; %dACC backward connection on laPFCsal
+    DCM.A{backward}(raPFCsal,dACC) = 1; %dACC backward connection on raPFCsal
+    DCM.A{backward}(lLPsal,laPFCsal) = 1; %laPFCsal backward connection on lLPsal
+    DCM.A{backward}(rLPsal,raPFCsal) = 1; %raPFCsal backward connection on rLPsal
+    DCM.A{backward}(lSP,laPFCcen) = 1; %laPFCcen backward connection on lSP
+    DCM.A{backward}(rSP,raPFCcen) = 1; %raPFCcen backward connection on rSP
+    DCM.A{backward}(lSP,dmPFC) = 1; %dmPFC backward connection on lSP
+    DCM.A{backward}(laPFCcen,dmPFC) = 1; %dmPFC backward connection on laPFCcen
+    DCM.A{backward}(rSP,dmPFC) = 1; %dmPFC backward connection on rSP
+    DCM.A{backward}(raPFCcen,dmPFC) = 1; %dmPFC backward connection on raPFCcen
+    
+    DCM.A{lateral} = zeros(Nareas,Nareas);
+    DCM.A{lateral}(rLPdmn,lLPdmn) = 1; %lLPdmn lateral connection to rLPdmn
+    DCM.A{lateral}(lLPdmn,rLPdmn) = 1; %rLPdmn lateral connection to lLPdmn
+    DCM.A{lateral}(rLPsal,lLPsal) = 1; %lLPsal lateral connection to rLPsal
+    DCM.A{lateral}(lLPsal,rLPsal) = 1; %rLPsal lateral connection to lLPsal
+    DCM.A{lateral}(raPFCsal,laPFCsal) = 1; %laPFCsal lateral connection to raPFCsal
+    DCM.A{lateral}(laPFCsal,raPFCsal) = 1; %raPFCsal lateral connection to laPFCsal
+    DCM.A{lateral}(rSP,lSP) = 1; %lSP lateral connection to rSP
+    DCM.A{lateral}(lSP,rSP) = 1; %rSP lateral connection to lSP
+    DCM.A{lateral}(raPFCcen,laPFCcen) = 1; %laPFCcen lateral connection to raPFCcen
+    DCM.A{lateral}(laPFCcen,raPFCcen) = 1; %raPFCcen lateral connection to laPFCcen
+    
+    %Between Network Connection
+    DCM.A{forward}(lSP,Prec) = 1; %CEN-DMN
+    DCM.A{forward}(rSP,Prec) = 1; %CEN-DMN
+    DCM.A{forward}(Prec,dACC) = 1; %DMN-SAL
+    
+    DCM.A{backward}(Prec,lSP) = 1; %DMN-CEN
+    DCM.A{backward}(Prec,rSP) = 1; %DMN-CEN
+    DCM.A{backward}(dACC,Prec) = 1; %SAL-DMN
+    
+    %Every connection is allowed to change
+    DCM.B{1} = DCM.A{1} + DCM.A{2} + DCM.A{3};
+    DCM.C = sparse(length(DCM.A{1}),0); %Added C so no error in PEB
+    
 end
+
 %--------------------------------------------------------------------------
 %%
 
@@ -172,8 +281,13 @@ DCM.options.spatial  = 'IMG';  % spatial model (forward model)
 DCM.options.trials   = [1 2]; 
 DCM.options.Tdcm(1)  = 0;      % start of peri-stimulus time to be modelled (ms)
 DCM.options.Tdcm(2)  = 10000;  % end of peri-stimulus time to be modelled (ms)
-DCM.options.Nmodes   = 8;      % number of spatial modes to invert (10 for large 
-                               % scale in Razi et al.(2017)
+
+if modindex == 4
+    DCM.options.Nmodes   = 14;      % number of spatial modes to invert 
+else
+    DCM.options.Nmodes   = 8; 
+end
+
 DCM.options.h        = 1;      % number of DCT components
 %DCM.options.onset   = 100;    % selection of onset (prior mean)
 DCM.options.D        = 1;      % down-sampling
@@ -195,6 +309,6 @@ DCM.options.DATA = 0;
 % Fitting of DCM models
 
 fprintf('\n\n\n*** Processing model %d (%s) ***\n\n\n', modindex, models{modindex});
-% DCM = spm_dcm_csd(DCM); % Estimate parameters of a DCM (complex) cross-spectral density
+DCM = spm_dcm_csd(DCM); % Estimate parameters of a DCM (complex) cross-spectral density
 save([datafile '_model' (models{modindex}) '.mat'],'DCM')
 end
